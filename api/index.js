b7ed6193e9a75318c7300b3aa21f341ad1af5c86
Mei0-Metr0 <meiometro.mail@gmail.com>
2024-06-29 11:45:12 -0300

    feat: Add database connection

--- api/index.js
+++ api/index.js
@@ -1,10 +1,19 @@
 import express from "express"
 import cors from "cors"
+import dotenv from "dotenv"
+import connectDatabase from './db/conn.js';
+
+dotenv.config()
+
+const port = process.env.DB_PORT
 
 const app = express()
 app.use(cors())
 app.use(express.json())
 
-app.listen(3000, function() {
-    console.log("Servidor rodando na porta 3000");
+//DB Connection
+connectDatabase()
+
+app.listen(port, function() {
+    console.log(`Servidor rodando na porta ${port}`);
 })
