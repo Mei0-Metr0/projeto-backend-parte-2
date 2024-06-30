8fbd6811b5c62a04dcca51ed1d16307ac5c80f9a
Mei0-Metr0 <meiometro.mail@gmail.com>
2024-06-29 23:25:13 -0300

    feat: Add login with jwt

--- api/routes/routes.js
+++ api/routes/routes.js
@@ -74,4 +74,59 @@ router.post('/auth/register', async(req, res) => {
     }
 })
 
+//Login user
+router.post('/auth/login', async(req, res) => {
+    const { email, password } = req.body
+
+    //validations
+    if(!email) {
+        return res.status(422).json({
+            msg: "Email is required"
+        })
+    }
+
+    if(!password) {
+        return res.status(422).json({
+            msg: "Password is required"
+        })
+    }
+
+    //check if user exist
+    const user = await User.findOne({ email: email})
+
+    if(!user) {
+        return res.status(404).json({
+            msg: "User not found"
+        })
+    }
+
+    //check if password match
+    const checkPassword = await bcrypt.compare(password, user.password)
+
+    if(!checkPassword) {
+        return res.status(422).json({
+            msg: 'Invalid password'
+        })
+    }
+
+    try{
+        const secret = process.env.SECRET
+        const token = jwt.sign(
+            {
+                id: user._id,
+            },
+            secret,
+        )
+
+        res.status(200).json({
+            msg: "Authentication completed successfully", token
+        })
+    }catch(err){
+        console.log(err)
+        return res.status(500).json({
+            msg: "A server error occurred, please try again later"
+        })
+    }
+})
+
 export default router;
