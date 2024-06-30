ef3af112dc78f30b51e0ce5cccedc712b8745345
Mei0-Metr0 <meiometro.mail@gmail.com>
2024-06-29 23:57:08 -0300

    feat: Add test private route

--- api/routes/routes.js
+++ api/routes/routes.js
@@ -12,6 +12,50 @@ router.get('/', (req, res) => {
     })
 });
 
+// Private Route
+router.get('/user/:id', checkToken, async (req, res) => {
+
+    const id = req.params.id
+
+    //check if user exists
+    const user = await User.findById(id, '-password')
+
+    if(!user) {
+        return res.status(404).json({
+            msg: "User not found"
+        })
+    }
+
+    res.status(200).json({
+        user
+    })
+});
+
+function checkToken(req, res, next) {
+
+    const authHeader = req.headers['authorization']
+    const token = authHeader && authHeader.split(" ")[1]
+
+    if(!token) {
+        return res.status(401).json({
+            msg: "Access denied"
+        })
+    }
+
+    try {
+        const secret = process.env.SECRET
+
+        jwt.verify(token, secret)
+
+        next()
+    } catch(error) {
+        res.status(400).json({
+            msg: "Invalid token"
+        })
+    }
+
+}
+
 //Register User
 router.post('/auth/register', async(req, res) => {
     const {name, email, password, confirmpassword} = req.body
